19c32ffc136712b6481d4f24a4690586c32d8fbd
G Locke <jkeys726@gmail.com>
2023-11-10 23:14:04 -0600

    added some HTML

--- trashapp/main/urls.py
+++ trashapp/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("<str:name>", views.index, name="index"),
+  path("<int:id>", views.index, name="index"),
+  path("", views.home, name="home"),
 ]
--- trashapp/main/views.py
+++ trashapp/main/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 # Create your views here.
 
-def index(response, name):
-  ls = ToDoList.objects.get(name=name)
-  return HttpResponse("<h1>%s</h1>" % ls.name)
+def index(response, id):
+  ls = ToDoList.objects.get(id=id)
+  return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
