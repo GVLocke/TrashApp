30af17841c8be2afa86f4c702749430ee3a1fb62
G Locke <jkeys726@gmail.com>
2023-11-11 01:48:37 -0600

    fixed data reception

--- trashapp/main/views.py
+++ trashapp/main/views.py
@@ -1,28 +1,37 @@
 import json
+import re
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from .models import DistanceMeasurement
+from django.http import HttpResponse
+
+from django.http import JsonResponse
 
 @csrf_exempt
 def receive_distance_data(request):
     if request.method == 'POST':
+        serial_data = request.POST.get('serial_data')
+
+        # Check if serial_data is None
+        if serial_data is None:
+            return JsonResponse({'error': 'No serial_data in request'}, status=400)
+
+        # Try to convert serial_data to a float
         try:
-            data = json.loads(request.body.decode('utf-8'))
-            distance_value = data.get('distance')
+            distance = float(serial_data)
+        except ValueError:
+            return JsonResponse({'error': 'serial_data could not be converted to a float'}, status=400)
 
-            if distance_value is not None:
-                DistanceMeasurement.objects.create(distance=distance_value)
-                return JsonResponse({'status': 'success'})
-            else:
-                return JsonResponse({'status': 'error', 'message': 'Invalid data'})
-        except json.JSONDecodeError:
-            return JsonResponse({'status': 'error', 'message': 'Invalid JSON data'})
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
-    
+        DistanceMeasurement.objects.create(distance=distance)
+        return JsonResponse({'status': 'Success'})
+
+    return JsonResponse({'error': 'Method not allowed'}, status=405)
+
 def main_page(request):
-    latest_measurment = DistanceMeasurement.objects.last()
+    # Get the latest DistanceMeasurement object
+    latest_measurement = DistanceMeasurement.objects.order_by('-created_at').first()
 
-    context = {'latest_measurment': latest_measurment}
+    # Pass the latest_measurement to the template
+    context = {'latest_measurement': latest_measurement}
     return render(request, 'main/mainpage.html', context)
