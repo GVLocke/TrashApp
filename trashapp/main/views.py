d26cf00e6c2871465c1e26b433b1e43bd7e5e5b1
G Locke <jkeys726@gmail.com>
2023-11-11 11:09:06 -0600

    Add TrashCan model and update DistanceMeasurement model to include foreign key to TrashCan. Also update views to receive trash can name in POST request.

--- trashapp/main/views.py
+++ trashapp/main/views.py
@@ -3,7 +3,7 @@ import re
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from .models import DistanceMeasurement
+from .models import DistanceMeasurement, TrashCan
 from django.http import HttpResponse
 
 from django.http import JsonResponse
@@ -11,19 +11,23 @@ from django.http import JsonResponse
 @csrf_exempt
 def receive_distance_data(request):
     if request.method == 'POST':
+        trash_can_name = request.POST.get('trash_can')
         serial_data = request.POST.get('serial_data')
 
-        # Check if serial_data is None
-        if serial_data is None:
-            return JsonResponse({'error': 'No serial_data in request'}, status=400)
+        if trash_can_name is None or serial_data is None:
+            return JsonResponse({'error': 'Missing trash_can or serial_data in request'}, status=400)
 
-        # Try to convert serial_data to a float
+        try:
+            trash_can = TrashCan.objects.get(name=trash_can_name)
+        except TrashCan.DoesNotExist:
+            return JsonResponse({'error': 'Trash can does not exist'}, status=400)
+
         try:
             distance = float(serial_data)
         except ValueError:
             return JsonResponse({'error': 'serial_data could not be converted to a float'}, status=400)
 
-        DistanceMeasurement.objects.create(distance=distance)
+        DistanceMeasurement.objects.create(distance=distance, trash_can=trash_can)
         return JsonResponse({'status': 'Success'})
 
     return JsonResponse({'error': 'Method not allowed'}, status=405)
