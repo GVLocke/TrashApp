53fdc75bc119e745883cc80c94dbebed5b2b059c
James Keys <102273198+GVLocke@users.noreply.github.com>
2023-11-11 02:45:06 +0000

    oops

--- main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import ToDoList, Item
-# Create your views here.
-
-def index(response, name):
-  ls = ToDoList.objects.get(name=name)
-  return HttpResponse("<h1>%s</h1>" % ls.name)
